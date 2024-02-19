0767e2b02eaf1ca39e30a19a7e262b66f1cbfa59
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-02-20 00:01:57 +0100

    Modulos PrimeNG necesarios

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
-import { MenuModule } from 'primeng/menu';
+// import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
-
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import {  PanelModule} from "primeng/panel";
 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,17 +13,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    ButtonModule,
-    MenuModule,
-    BrowserModule,
+    // MenuModule,
     BrowserAnimationsModule,
+    BrowserModule,
+    ButtonModule,
+    CommonModule,
     MenubarModule,
+
   ],
   exports:[
+    // MenuModule,
     ButtonModule,
-    MenuModule,
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
     MenubarModule,
+    PanelModule,
   ]
 })
 export class PrimeNGModule { }
